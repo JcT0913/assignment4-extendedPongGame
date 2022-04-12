5f987cf3df839f50961803aa836b12b8faec9f95
Jiachen Tang <tjc9606ok@gmail.com>
2022-04-12 12:57:23 -0400

    script for ball movement added & box setting modified

---
+++ assignment 4 Extended Pong Game/Assets/BallMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartOfBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartOfBall()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float x = Random.Range(-5f, 5f);
+        float y = Random.Range(-10f, -5f);
+        Vector3 force = new Vector3(x, y, 0);
+
+        rigidbody.velocity = new Vector3(0, 0, 0);
+        rigidbody.AddForce(force.normalized * speed);
+    }
+}
