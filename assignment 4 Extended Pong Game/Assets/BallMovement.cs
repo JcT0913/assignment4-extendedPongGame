bfe91f4a6c9409cfa30ee785bad16400752a8793
Jiachen Tang <tjc9606ok@gmail.com>
2022-04-13 05:52:04 -0400

    code for dictionary and list added & Restart Level script connected

--- assignment 4 Extended Pong Game/Assets/BallMovement.cs
+++ assignment 4 Extended Pong Game/Assets/BallMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     public float speed = 4;
+    public static Dictionary<string, List<Transform>> repositoryDict = new Dictionary<string, List<Transform>>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,7 @@ public class BallMovement : MonoBehaviour
 
         float x = Random.Range(-5f, 5f);
         float y = Random.Range(-10f, -5f);
+        
         Vector3 force = new Vector3(x, y, 0);
 
         rigidbody.velocity = new Vector3(0, 0, 0);
@@ -34,5 +36,30 @@ public class BallMovement : MonoBehaviour
     {
         transform.position = new Vector3(-4.4f, 0, 0);
         //StartOfBall();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        List<Transform> list;
+        //GetComponent<Rigidbody>().detectCollisions = false;
+        BallMovement.repositoryDict.TryGetValue(collision.transform.tag, out list);
+
+        int elementsInAllListsCount = 0;
+
+        foreach (List<Transform> li in BallMovement.repositoryDict.Values)
+        {
+            elementsInAllListsCount += li.Count;
+        }
+
+        if (list == null)
+        {
+            list = new List<Transform>();
+        }
+
+        list.Add(collision.transform);
+        BallMovement.repositoryDict.Remove(collision.transform.tag);
+        BallMovement.repositoryDict.Add(collision.transform.tag, list);
+        //Debug.Log(list.Count);
+        //Debug.Log(elementsInAllListsCount);
     }
 }
