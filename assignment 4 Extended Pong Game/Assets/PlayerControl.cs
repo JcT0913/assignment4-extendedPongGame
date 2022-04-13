a20227b4b16314fd8ec1acdc6d5f20626a13c2b6
Jiachen Tang <tjc9606ok@gmail.com>
2022-04-13 00:53:19 -0400

    Player Control script completed & On Collision script connected

--- assignment 4 Extended Pong Game/Assets/PlayerControl.cs
+++ assignment 4 Extended Pong Game/Assets/PlayerControl.cs
@@ -15,6 +15,22 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -8.17f)
+        {
+            transform.Translate(new Vector3(speed * -0.025f, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.D) && transform.position.x < -1.01f)
+        {
+            transform.Translate(new Vector3(speed * 0.025f, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.W) && transform.position.y < -1f)
+        {
+            transform.Translate(new Vector3(0, speed * 0.025f, 0));
+        }
+        else if (Input.GetKey(KeyCode.S) && transform.position.y > -4.61f)
+        {
+            transform.Translate(new Vector3(0, speed * -0.025f, 0));
+        }
     }
 }
